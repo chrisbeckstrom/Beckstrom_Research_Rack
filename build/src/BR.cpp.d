2ffda707bc9cdbaddb6c26937d4cca3de4d22b31
Chris Beckstrom <chris@chrisbeckstrom.com>
2018-09-24 11:30:00 -0400

    redid everything to work with Rack 0.6

--- build/src/BR.cpp.d
+++ build/src/BR.cpp.d
@@ -1,10 +1,53 @@
-build/src/BR.cpp.o: src/BR.cpp src/BR.hpp ../../include/rack.hpp \
-  ../../include/util.hpp ../../include/math.hpp ../../include/asset.hpp \
-  ../../include/plugin.hpp ../../include/engine.hpp \
-  ../../dep/include/jansson.h ../../dep/include/jansson_config.h \
-  ../../include/gui.hpp ../../include/app.hpp ../../include/widgets.hpp \
-  ../../include/../ext/nanovg/src/nanovg.h \
-  ../../include/../ext/oui-blendish/blendish.h \
-  ../../include/../ext/nanosvg/src/nanosvg.h ../../include/events.hpp \
-  ../../dep/include/GL/glew.h ../../dep/include/GLFW/glfw3.h \
-  ../../include/components.hpp
+build/src/BR.cpp.o: src/BR.cpp src/BR.hpp src/BR_components.hpp \
+  ../../include/rack.hpp ../../include/util/common.hpp \
+  ../../include/util/math.hpp ../../include/asset.hpp \
+  ../../include/plugin.hpp ../../include/tags.hpp \
+  ../../include/engine.hpp ../../dep/include/jansson.h \
+  ../../dep/include/jansson_config.h ../../include/widgets.hpp \
+  ../../dep/include/nanovg.h ../../dep/include/nanosvg.h \
+  ../../include/events.hpp ../../include/util/color.hpp \
+  ../../include/app.hpp ../../include/ui.hpp \
+  ../../dep/include/blendish.h ../../include/componentlibrary.hpp \
+  ../../include/helpers.hpp
+
+src/BR.hpp:
+
+src/BR_components.hpp:
+
+../../include/rack.hpp:
+
+../../include/util/common.hpp:
+
+../../include/util/math.hpp:
+
+../../include/asset.hpp:
+
+../../include/plugin.hpp:
+
+../../include/tags.hpp:
+
+../../include/engine.hpp:
+
+../../dep/include/jansson.h:
+
+../../dep/include/jansson_config.h:
+
+../../include/widgets.hpp:
+
+../../dep/include/nanovg.h:
+
+../../dep/include/nanosvg.h:
+
+../../include/events.hpp:
+
+../../include/util/color.hpp:
+
+../../include/app.hpp:
+
+../../include/ui.hpp:
+
+../../dep/include/blendish.h:
+
+../../include/componentlibrary.hpp:
+
+../../include/helpers.hpp:
